cb9852c5d0d287aa91635dc3e712cd62e80013f0
AryanMahajan <mahajanaryan0409@gmail.com>
2025-06-11 10:15:31 +0530

    removed

--- credentials/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Credential(models.Model):
-    email = models.EmailField(unique=True, max_length=320)
-    password = models.CharField(max_length=1024)
-
-    def __str__(self):
-        return self.email
--- credentials/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Credential
-
-class CredentialSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Credential
-        fields = ['email', 'password']
