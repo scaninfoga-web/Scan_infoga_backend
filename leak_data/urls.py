1ff6294e39d110c5ec9cb0ba4b5958a89834ac7c
abhinav-2125 <abhinavsrivastava@scaninfoga.in>
2025-06-12 15:14:40 +0530

    updated code

--- leak_data/urls.py
+++ leak_data/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-password', views.get_passwords, name='get-passwords'),
+    # path('get-password', views.get_passwords, name='get-passwords'),
 ]
--- leak_data/views.py
+++ leak_data/views.py
@@ -1,18 +1,18 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.permissions import IsAuthenticated
 
-from core.utils import create_response
-from .utils import LeakEmailPasswords
+# from core.utils import create_response
+# from .utils import LeakEmailPasswords
 
-@api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-def get_passwords(request):
-    email = request.data.get('email')
-    if not email:
-        return Response(create_response(False, "Email is required", None), status=status.HTTP_400_BAD_REQUEST)
-    leak_email_passwords = LeakEmailPasswords()
-    passwords = leak_email_passwords.fetch_passwords(email)
-    return Response(create_response(True, "Data fetched from Database.", passwords), status=status.HTTP_200_OK)
+# @api_view(['GET'])
+# # @permission_classes([IsAuthenticated])
+# def get_passwords(request):
+#     email = request.data.get('email')
+#     if not email:
+#         return Response(create_response(False, "Email is required", None), status=status.HTTP_400_BAD_REQUEST)
+#     leak_email_passwords = LeakEmailPasswords()
+#     passwords = leak_email_passwords.fetch_passwords(email)
+#     return Response(create_response(True, "Data fetched from Database.", passwords), status=status.HTTP_200_OK)
 
